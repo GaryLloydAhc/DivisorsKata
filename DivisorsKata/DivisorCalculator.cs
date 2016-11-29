7e6213bc8b95cabbeeb9eb1e09f90d02b3696ed3
ramithaprasad <ramitha.prasad@oneadvanced.com>
2016-11-29 16:01:54 +0530

    Refactored the code and completed the kata to pass all the tests

--- DivisorsKata/DivisorCalculator.cs
+++ DivisorsKata/DivisorCalculator.cs
@@ -7,24 +7,15 @@ namespace DivisorsKata
     {
         public int[] Calculate(int inputNumber)
         {
-            var divisors = new List<int> {1};
+            List<int> list = new List<int>();
+            var divisors = Enumerable.Range(1, inputNumber).Where((x) => inputNumber%x == 0);
 
-            if (inputNumber % 2 == 0)          
-                divisors.AddRange(new List<int> {2, inputNumber / 2});
+            foreach (var div in divisors)
+            {
+                list.Add(div);
+            }
 
-            if (inputNumber % 3 == 0)
-                divisors.AddRange(new List<int> { 3, inputNumber / 3 });
-
-            if (inputNumber % 4 == 0)
-                divisors.AddRange(new List<int> { 4, inputNumber / 4 });
-
-            if (inputNumber % 6 == 0)
-                divisors.AddRange(new List<int> { 6, inputNumber / 6 });
-
-            divisors.Add(inputNumber);
-
-            divisors.Sort();
-            return divisors.Distinct().ToArray();
+            return list.ToArray();
         }
     }
 }
